92011bf77d9e98b013f403c07b4a3112e6169c58
Jia Xi Shan <mercuries018@gmail.com>
2026-02-20 13:05:37 +0100

    Añadió el mensaje de bienvenida, botón de modo oscuro y boton de hacer la receta aleatoria

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-
+// MENSAJE DE BIENVENIDA
+alert("Bienvenido al página de recetas");
+// BOTÓN MODO OSCURO
+var botonModo = document.getElementById("modoBtn");
+botonModo.onclick = function() {    
+    document.body.classList.toggle("oscuro");
+    alert("Has cambiado el modo de la página");
+};
+// RECETA ALEATORIA
+var botonReceta = document.getElementById("recetaBtn");
+botonReceta.onclick = function() {
+    var numero = Math.floor(Math.random() * 3);
+    if (numero == 0) {
+        alert("Hoy puedes cocinar: Tortilla de Patatas");
+    }
+    if (numero == 1) {
+        alert("Hoy puedes cocinar: Ensalada Mixta");
+    }
+    if (numero == 2) {
+        alert("Hoy puedes cocinar: Bizcocho Casero");
+    }
+};
